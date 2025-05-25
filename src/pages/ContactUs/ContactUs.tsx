6489833756888185fc9438b2f8622a3de48c8eb5
hasmanukyan <hasmikman2004@gmail.com>
2025-05-25 14:57:07 +0400

    Changed Location map from russian to english

--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -36,7 +36,7 @@ const ContactUs: React.FC = () => {
       <div className="contact-map">
         <iframe
           title="Hospital Location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3047.870808361706!2d44.52534637604502!3d40.189688471476984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abcdebf38f295%3A0xcda0897b2ca7158f!2sArmenian-American%20Wellness%20Center!5e0!3m2!1sru!2sam!4v1748152128582!5m2!1sru!2sam"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3047.8708070799626!2d44.5253464!3d40.1896885!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abcdebf38f295%3A0xcda0897b2ca7158f!2sArmenian-American%20Wellness%20Center!5e0!3m2!1sen!2sam!4v1748169017863!5m2!1sen!2sam"
           width="100%"
           height="400"
           style={{ border: 0 }}
