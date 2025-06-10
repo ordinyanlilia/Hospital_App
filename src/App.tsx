2e3f55894d06890dfcb26bfb6358dccc09cb7061
hasmanukyan <hasmikman2004@gmail.com>
2025-06-10 22:25:37 +0400

    Medical chat box translation added

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,15 @@ import {RouterProvider} from "react-router-dom";
 import {router} from "./routes/routes";
 import OnikBot from "./components/OnikBot";
 import AuthLoader from "./pages/Auth/AuthLoader";
-import { TranslationProvider } from "./context/TranslationProvider";
 
 
 const App = () => {
     return (
         <>
-            <TranslationProvider>
-                <AuthLoader>
-                    <RouterProvider router={router}/>
-                    <OnikBot/>
-                </AuthLoader>
-            </TranslationProvider>
+            <AuthLoader>
+                <RouterProvider router={router}/>
+                <OnikBot/>
+            </AuthLoader>
         </>
     );
 };
