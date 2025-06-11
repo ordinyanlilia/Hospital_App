021d340ef5ca7e974ca8643b72fd84f4feccefe8
Lilia Ordinyan <lilia.ordinyan@gmail.com>
2025-06-12 01:06:11 +0400

    Home  darklight fixed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,26 @@
-import { Layout } from 'antd';
-import HeroSection from './HeroSection';
-import ServicesSection from './ServiceSection';
-import AboutSection from './AboutSection';
-import ContactSection from './ContactSection';
-import WelcomeBanner from './WelcomeBanner';
-import { Footer } from '../../components/Footer';
-import './Home.css'; // Import your CSS here
+import { Layout } from "antd";
+import HeroSection from "./HeroSection";
+import ServicesSection from "./ServiceSection";
+import AboutSection from "./AboutSection";
+import ContactSection from "./ContactSection";
+import WelcomeBanner from "./WelcomeBanner";
+import { Footer } from "../../components/Footer";
+import { useTheme } from "../../context/theme-context";
+import "./Home.css";
 
 const { Content } = Layout;
 
 const Home = () => {
+  const { darkMode } = useTheme();
   return (
-    <Layout className="home-layout">
+    <Layout className={`home-layout ${darkMode ? "dark-mode" : ""}`}>
       <Content className="home-content">
         <WelcomeBanner />
         <HeroSection />
         <ServicesSection />
         <AboutSection />
         <ContactSection />
-        <Footer />
+        {/* <Footer /> */}
       </Content>
     </Layout>
   );
