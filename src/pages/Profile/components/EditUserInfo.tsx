00f06527cbadc1ff8bd4941b72cd531690cbc973
susannasardaryan <susannasardaryann@gmail.com>
2025-06-12 00:41:56 +0400

    feat: added current medications, fixed design

--- src/pages/Profile/components/EditUserInfo.tsx
+++ src/pages/Profile/components/EditUserInfo.tsx
@@ -1,274 +1,253 @@
-import {
-  Button,
-  Col,
-  DatePicker,
-  Flex,
-  Input,
-  List,
-  Row,
-  Select,
-  Space,
-  Typography,
-} from "antd";
-import {
-  DeleteOutlined,
-  DeleteTwoTone,
-  MailOutlined,
-  PhoneOutlined,
-} from "@ant-design/icons";
-import { type ChangeEvent, useState } from "react";
+import {Button, Col, DatePicker, Flex, Input, List, Row, Select, Space, Typography,} from "antd";
+import {DeleteTwoTone, MailOutlined, PhoneOutlined,} from "@ant-design/icons";
+import {type ChangeEvent, useState} from "react";
 import dayjs from "dayjs";
 import ImgUploader from "./ImgUploader.tsx";
-import {
-  editPatient,
-  type Patient,
-  selectPatient,
-} from "../../../features/PatientSlice.ts";
-import { useAppDispatch, useAppSelector } from "../../../app/hooks.ts";
-import { useTheme } from "../../../context/theme-context.tsx";
+import {editPatient, type Patient, selectPatient,} from "../../../features/PatientSlice.ts";
+import {useAppDispatch, useAppSelector} from "../../../app/hooks.ts";
+import {useTheme} from "../../../context/theme-context.tsx";
 
-const { Text } = Typography;
-const { Option } = Select;
+const {Text} = Typography;
+const {Option} = Select;
 
-const EditUserInfo = ({ onSetIsEditing }: { onSetIsEditing: () => void }) => {
-  const user: Patient | null = useAppSelector(selectPatient);
-  const dispatch = useAppDispatch();
-  const { darkMode } = useTheme();
-  const [formData, setFormData] = useState({
-    name: user?.name || "",
-    surname: user?.surname || "",
-    phoneNumber: user?.phoneNumber || "",
-    email: user?.email || "",
-    gender: user?.gender || "",
-    dob: user?.dob || "",
-    bloodType: user?.bloodType || "",
-    allergies: user?.allergies || [],
-    currentMedications: user?.currentMedications || [],
-    imageUrl: user?.imageUrl || "",
-  });
+const EditUserInfo = ({onSetIsEditing}: { onSetIsEditing: () => void }) => {
+    const user: Patient | null = useAppSelector(selectPatient);
+    const dispatch = useAppDispatch();
+    const {darkMode} = useTheme();
+    const [formData, setFormData] = useState({
+        name: user?.name || "",
+        surname: user?.surname || "",
+        phoneNumber: user?.phoneNumber || "",
+        email: user?.email || "",
+        gender: user?.gender || "",
+        dob: user?.dob || "",
+        bloodType: user?.bloodType || "",
+        allergies: user?.allergies || [],
+        imageUrl: user?.imageUrl || "",
+    });
 
-  const handleChange =
-    (field: string) => (e: ChangeEvent<HTMLInputElement>) => {
-      const value = e?.target?.value ?? e;
-      setFormData((prev) => ({ ...prev, [field]: value }));
+    const handleChange =
+        (field: string) => (e: ChangeEvent<HTMLInputElement>) => {
+            const value = e?.target?.value ?? e;
+            setFormData((prev) => ({...prev, [field]: value}));
+        };
+
+    const handleListChange = (
+        field: "allergies",
+        index: number,
+        value: string
+    ) => {
+        const updatedList = [...formData[field]];
+        updatedList[index] = value;
+        setFormData((prev) => ({...prev, [field]: updatedList}));
     };
 
-  const handleListChange = (
-    field: "allergies" | "currentMedications",
-    index: number,
-    value: string
-  ) => {
-    const updatedList = [...formData[field]];
-    updatedList[index] = value;
-    setFormData((prev) => ({ ...prev, [field]: updatedList }));
-  };
+    const handleAddListItem = (field: "allergies") => {
+        setFormData((prev) => ({...prev, [field]: [...prev[field], ""]}));
+    };
 
-  const handleAddListItem = (field: "allergies" | "currentMedications") => {
-    setFormData((prev) => ({ ...prev, [field]: [...prev[field], ""] }));
-  };
+    const handleDeleteListItem = (
+        field: "allergies",
+        index: number
+    ) => {
+        const updatedList = formData[field].filter(
+            (_item: string, i: number) => i !== index
+        );
+        setFormData((prev) => ({...prev, [field]: updatedList}));
+    };
 
-  const handleDeleteListItem = (
-    field: "allergies" | "currentMedications",
-    index: number
-  ) => {
-    const updatedList = formData[field].filter(
-      (_item: string, i: number) => i !== index
+    const handleSave = async () => {
+        if (user && user?.doc_id) {
+            dispatch(
+                editPatient({
+                    user_doc_id: user?.doc_id,
+                    data: formData,
+                })
+            );
+        }
+        onSetIsEditing();
+    };
+
+    const handleDateChange = (date: dayjs.Dayjs) => {
+        if (date) {
+            setFormData((prev) => ({...prev, dob: date.toISOString()}));
+        }
+    };
+
+    const handleImageFileChange = (url: string) => {
+        setFormData((prev) => ({...prev, imageUrl: url}));
+    };
+
+    return (
+        <Row
+            gutter={[16, 16]}
+            className="profile"
+            style={{background: darkMode ? "#101832" : "#f5f5f5"}}
+        >
+            <Col span={12}>
+                <Flex vertical gap={"small"}>
+                    <ImgUploader
+                        imageUrl={formData.imageUrl}
+                        onSetFormData={handleImageFileChange}
+                    />
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Name
+                    </Text>
+                    <Input
+                        placeholder="Name"
+                        value={formData.name}
+                        onChange={handleChange("name")}
+                    />
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Surname
+                    </Text>
+                    <Input
+                        placeholder="Surname"
+                        value={formData.surname}
+                        onChange={handleChange("surname")}
+                    />
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Phone Number
+                    </Text>
+                    <Input
+                        prefix={<PhoneOutlined/>}
+                        placeholder="Phone Number"
+                        value={formData.phoneNumber}
+                        onChange={handleChange("phoneNumber")}
+                    />
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Email
+                    </Text>
+                    <Input
+                        prefix={<MailOutlined/>}
+                        placeholder="Email"
+                        value={formData.email}
+                        onChange={handleChange("email")}
+                    />
+                    <Space align={"center"}>
+                        <Button onClick={handleSave}>Save</Button>
+                        <Button
+                            onClick={() => onSetIsEditing()}
+                            danger
+                            style={{marginLeft: 8}}
+                        >
+                            Cancel
+                        </Button>
+                    </Space>
+                </Flex>
+            </Col>
+
+            <Col span={12}>
+                <div className="profile-details">
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Gender
+                    </Text>
+                    <Select
+                        value={formData.gender}
+                        onChange={(value) =>
+                            setFormData((prev) => ({...prev, gender: value}))
+                        }
+                        style={{width: "100%"}}
+                    >
+                        <Option value="male">Male</Option>
+                        <Option value="female">Female</Option>
+                        <Option value="other">Other</Option>
+                    </Select>
+
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Date of Birth
+                    </Text>
+                    <DatePicker
+                        value={dayjs(formData.dob, "YYYY/MM/DD")}
+                        format={"YYYY/MM/DD"}
+                        onChange={handleDateChange}
+                        style={{width: "100%"}}
+                    />
+
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Blood Type
+                    </Text>
+                    <Input
+                        placeholder="Blood Type"
+                        value={formData.bloodType}
+                        onChange={handleChange("bloodType")}
+                    />
+
+                    <Text type="secondary" strong className={"blue-text"}>
+                        Allergies
+                    </Text>
+                    <List
+                        className={"edit-list"}
+                        dataSource={formData.allergies}
+                        renderItem={(item, index) => (
+                            <List.Item
+                                key={index}
+                                style={{
+                                    border: "none",
+                                    padding: "8px",
+                                }}
+                            >
+                                <Input
+                                    size={"middle"}
+                                    value={item}
+                                    onChange={(e) =>
+                                        handleListChange("allergies", index, e.target.value)
+                                    }
+                                />
+                                <DeleteTwoTone
+                                    onClick={() => handleDeleteListItem("allergies", index)}
+                                    style={{cursor: "pointer", marginLeft: 8}}
+                                />
+                            </List.Item>
+                        )}
+                    />
+                    <Button onClick={() => handleAddListItem("allergies")}>
+                        Add Allergy
+                    </Button>
+                </div>
+            </Col>
+
+            {/*<Col span={8}>*/}
+            {/*  <div className="profile-history">*/}
+            {/*    <Text type="secondary" strong className={"blue-text"}>*/}
+            {/*      Current Medications*/}
+            {/*    </Text>*/}
+            {/*    <List*/}
+            {/*      dataSource={formData.currentMedications}*/}
+            {/*      renderItem={(item, index) => (*/}
+            {/*        <List.Item*/}
+            {/*          key={index}*/}
+            {/*          style={{*/}
+            {/*            border: "none",*/}
+            {/*            padding: "8px",*/}
+            {/*          }}*/}
+            {/*        >*/}
+            {/*          <Input*/}
+            {/*            value={item}*/}
+            {/*            onChange={(e) =>*/}
+            {/*              handleListChange(*/}
+            {/*                "currentMedications",*/}
+            {/*                index,*/}
+            {/*                e.target.value*/}
+            {/*              )*/}
+            {/*            }*/}
+            {/*          />*/}
+            {/*          <DeleteOutlined*/}
+            {/*            style={{ cursor: "pointer", marginLeft: 8 }}*/}
+            {/*            onClick={() =>*/}
+            {/*              handleDeleteListItem("currentMedications", index)*/}
+            {/*            }*/}
+            {/*          />*/}
+            {/*        </List.Item>*/}
+            {/*      )}*/}
+            {/*    />*/}
+            {/*    <Button onClick={() => handleAddListItem("currentMedications")}>*/}
+            {/*      Add Medication*/}
+            {/*    </Button>*/}
+            {/*  </div>*/}
+            {/*</Col>*/}
+        </Row>
     );
-    setFormData((prev) => ({ ...prev, [field]: updatedList }));
-  };
-
-  const handleSave = async () => {
-    if (user && user?.doc_id) {
-      dispatch(
-        editPatient({
-          user_doc_id: user?.doc_id,
-          data: formData,
-        })
-      );
-    }
-    onSetIsEditing();
-  };
-
-  const handleDateChange = (date: dayjs.Dayjs) => {
-    if (date) {
-      setFormData((prev) => ({ ...prev, dob: date.toISOString() }));
-    }
-  };
-
-  const handleImageFileChange = (url: string) => {
-    setFormData((prev) => ({ ...prev, imageUrl: url }));
-  };
-
-  return (
-    <Row
-      gutter={[16, 16]}
-      className="profile"
-      style={{ background: darkMode ? "#101832" : "#f5f5f5" }}
-    >
-      <Col span={8}>
-        <Flex vertical gap={"small"}>
-          <ImgUploader
-            imageUrl={formData.imageUrl}
-            onSetFormData={handleImageFileChange}
-          />
-          <Text type="secondary" strong className={"blue-text"}>
-            Name
-          </Text>
-          <Input
-            placeholder="Name"
-            value={formData.name}
-            onChange={handleChange("name")}
-          />
-          <Text type="secondary" strong className={"blue-text"}>
-            Surname
-          </Text>
-          <Input
-            placeholder="Surname"
-            value={formData.surname}
-            onChange={handleChange("surname")}
-          />
-          <Text type="secondary" strong className={"blue-text"}>
-            Phone Number
-          </Text>
-          <Input
-            prefix={<PhoneOutlined />}
-            placeholder="Phone Number"
-            value={formData.phoneNumber}
-            onChange={handleChange("phoneNumber")}
-          />
-          <Text type="secondary" strong className={"blue-text"}>
-            Email
-          </Text>
-          <Input
-            prefix={<MailOutlined />}
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange("email")}
-          />
-          <Space align={"center"}>
-            <Button onClick={handleSave}>Save</Button>
-            <Button
-              onClick={() => onSetIsEditing()}
-              danger
-              style={{ marginLeft: 8 }}
-            >
-              Cancel
-            </Button>
-          </Space>
-        </Flex>
-      </Col>
-
-      <Col span={8}>
-        <div className="profile-details">
-          <Text type="secondary" strong className={"blue-text"}>
-            Gender
-          </Text>
-          <Select
-            value={formData.gender}
-            onChange={(value) =>
-              setFormData((prev) => ({ ...prev, gender: value }))
-            }
-            style={{ width: "100%" }}
-          >
-            <Option value="male">Male</Option>
-            <Option value="female">Female</Option>
-            <Option value="other">Other</Option>
-          </Select>
-
-          <Text type="secondary" strong className={"blue-text"}>
-            Date of Birth
-          </Text>
-          <DatePicker
-            value={dayjs(formData.dob, "YYYY/MM/DD")}
-            format={"YYYY/MM/DD"}
-            onChange={handleDateChange}
-            style={{ width: "100%" }}
-          />
-
-          <Text type="secondary" strong className={"blue-text"}>
-            Blood Type
-          </Text>
-          <Input
-            placeholder="Blood Type"
-            value={formData.bloodType}
-            onChange={handleChange("bloodType")}
-          />
-
-          <Text type="secondary" strong className={"blue-text"}>
-            Allergies
-          </Text>
-          <List
-            className={"edit-list"}
-            dataSource={formData.allergies}
-            renderItem={(item, index) => (
-              <List.Item
-                key={index}
-                style={{
-                  border: "none",
-                  padding: "8px",
-                }}
-              >
-                <Input
-                  size={"middle"}
-                  value={item}
-                  onChange={(e) =>
-                    handleListChange("allergies", index, e.target.value)
-                  }
-                />
-                <DeleteTwoTone
-                  onClick={() => handleDeleteListItem("allergies", index)}
-                  style={{ cursor: "pointer", marginLeft: 8 }}
-                />
-              </List.Item>
-            )}
-          />
-          <Button onClick={() => handleAddListItem("allergies")}>
-            Add Allergy
-          </Button>
-        </div>
-      </Col>
-
-      <Col span={8}>
-        <div className="profile-history">
-          <Text type="secondary" strong className={"blue-text"}>
-            Current Medications
-          </Text>
-          <List
-            dataSource={formData.currentMedications}
-            renderItem={(item, index) => (
-              <List.Item
-                key={index}
-                style={{
-                  border: "none",
-                  padding: "8px",
-                }}
-              >
-                <Input
-                  value={item}
-                  onChange={(e) =>
-                    handleListChange(
-                      "currentMedications",
-                      index,
-                      e.target.value
-                    )
-                  }
-                />
-                <DeleteOutlined
-                  style={{ cursor: "pointer", marginLeft: 8 }}
-                  onClick={() =>
-                    handleDeleteListItem("currentMedications", index)
-                  }
-                />
-              </List.Item>
-            )}
-          />
-          <Button onClick={() => handleAddListItem("currentMedications")}>
-            Add Medication
-          </Button>
-        </div>
-      </Col>
-    </Row>
-  );
 };
 
 export default EditUserInfo;
--- src/pages/Profile/components/UserInfo.tsx
+++ src/pages/Profile/components/UserInfo.tsx
@@ -1,135 +1,154 @@
-import {
-  Avatar,
-  Button,
-  Col,
-  List,
-  message,
-  Row,
-  Space,
-  Spin,
-  Typography,
-} from "antd";
-import { MailOutlined, PhoneOutlined, UserOutlined } from "@ant-design/icons";
+import {Avatar, Button, Col, Descriptions, List, message, Modal, Row, Space, Spin, Typography,} from "antd";
+import {MailOutlined, PhoneOutlined, UserOutlined} from "@ant-design/icons";
 import dayjs from "dayjs";
-import type { MouseEventHandler } from "react";
-import { useAppSelector } from "../../../app/hooks.ts";
-import {
-  selectPatient,
-  selectPatientError,
-  selectPatientStatus,
-} from "../../../features/PatientSlice.ts";
-import { useTheme } from "../../../context/theme-context.tsx";
-const { Text } = Typography;
+import type {MouseEventHandler} from "react";
+import {useAppSelector} from "../../../app/hooks.ts";
+import {selectPatient, selectPatientError, selectPatientStatus,} from "../../../features/PatientSlice.ts";
+import {useTheme} from "../../../context/theme-context.tsx";
+
+const {Text} = Typography;
 
 const UserInfo = ({
-  onSetIsEditing,
-}: {
-  onSetIsEditing: MouseEventHandler<HTMLElement>;
+                      onSetIsEditing,
+                  }: {
+    onSetIsEditing: MouseEventHandler<HTMLElement>;
 }) => {
-  const user = useAppSelector(selectPatient);
-  const { darkMode } = useTheme();
-  const registeredDate = dayjs(user?.registeredAt).format("MMM D, YYYY");
-  const dobDate = dayjs(user?.dob).format("DD/MM/YYYY");
-  const [messageApi, contextHolder] = message.useMessage();
+    const user = useAppSelector(selectPatient);
+    const {darkMode} = useTheme();
+    const registeredDate = dayjs(user?.registeredAt).format("MMM D, YYYY");
+    const dobDate = dayjs(user?.dob).format("DD/MM/YYYY");
+    const [messageApi, contextHolder] = message.useMessage();
+    const [modal, contextHolder1] = Modal.useModal();
 
-  const status = useAppSelector(selectPatientStatus);
-  const error = useAppSelector(selectPatientError);
-  if (status === "loading") {
-    return <Spin fullscreen>Loading</Spin>;
-  } else if (status === "error") {
-    messageApi.open({
-      type: "error",
-      content: `${error}`,
-    });
-  }
 
-  return (
-    <Row
-      gutter={[16, 16]}
-      className={"profile"}
-      style={{ background: darkMode ? "#101832" : "#f5f5f5" }}
-    >
-      {contextHolder}
-      <Col span={8}>
-        <div className="profile-main-info">
-          {!!user?.imageUrl && (
-            <img src={user?.imageUrl} alt="profile picture" />
-          )}
-          {!user?.imageUrl && (
-            <Avatar
-              size={104}
-              style={{ backgroundColor: "rgba(96,150,186,0.75)" }}
-              icon={
-                <UserOutlined style={{ fontSize: "30px", color: "#fffefe" }} />
-              }
-            />
-          )}
-          <h3>
-            {user?.name} {user?.surname}
-          </h3>
-          <p>
-            <PhoneOutlined />
-            {user?.phoneNumber}
-          </p>
-          <p>
-            <MailOutlined />
-            {user?.email}
-          </p>
-          <Space direction="vertical">
-            <Text type="secondary" strong>
-              Registered At:{registeredDate}
-            </Text>
-            <Button onClick={onSetIsEditing}>Edit Profile</Button>
-          </Space>
-        </div>
-      </Col>
-      <Col span={8}>
-        <div className="profile-details">
-          <Text type="secondary" strong>
-            Gender
-          </Text>
-          <p>{user?.gender}</p>
-          <Text type="secondary" strong>
-            Date of Birth
-          </Text>
-          <p>{dobDate}</p>
-          <Text type="secondary" strong>
-            Blood
-          </Text>
-          <p>{user?.bloodType}</p>
-          <Text type="secondary" strong>
-            Allergies
-          </Text>
-          <List
-            size="small"
-            itemLayout={"horizontal"}
-            dataSource={user?.allergies}
-            renderItem={(item: string) => <List.Item>{item}</List.Item>}
-          />
-        </div>
-      </Col>
-      <Col span={6}>
-        <div className="profile-history">
-          <Text type="secondary" strong>
-            Medical History
-          </Text>
-          <List
-            size="small"
-            dataSource={user?.medicalHistory}
-            renderItem={(item: string) => <List.Item>{item}</List.Item>}
-          />
-          <Text type="secondary" strong>
-            Current Medications
-          </Text>
-          <List
-            size="small"
-            dataSource={user?.currentMedications}
-            renderItem={(item: string) => <List.Item>{item}</List.Item>}
-          />
-        </div>
-      </Col>
-    </Row>
-  );
+    const status = useAppSelector(selectPatientStatus);
+    const error = useAppSelector(selectPatientError);
+    if (status === "loading") {
+        return <Spin fullscreen>Loading</Spin>;
+    } else if (status === "error") {
+        messageApi.open({
+            type: "error",
+            content: `${error}`,
+        });
+    }
+
+    const handleSeeDescription = (medication: string, description: string) => {
+        modal.info({
+            title: medication,
+            content: (
+                <>
+                    {description}
+                </>
+            ),
+        });
+    }
+
+    return (
+        <Row
+            gutter={[16, 16]}
+            className={"profile"}
+            style={{background: darkMode ? "#101832" : "#ffffff"}}
+        >
+            {contextHolder}
+            {contextHolder1}
+            <Col span={8}>
+                <div className="profile-main-info">
+                    {!!user?.imageUrl && (
+                        <img src={user?.imageUrl} alt="profile picture"/>
+                    )}
+                    {!user?.imageUrl && (
+                        <Avatar
+                            size={104}
+                            style={{backgroundColor: "rgba(96,150,186,0.75)"}}
+                            icon={
+                                <UserOutlined style={{fontSize: "30px", color: "#fffefe"}}/>
+                            }
+                        />
+                    )}
+                    <h3>
+                        {user?.name} {user?.surname}
+                    </h3>
+                    <p>
+                        <PhoneOutlined/>
+                        {user?.phoneNumber}
+                    </p>
+                    <p>
+                        <MailOutlined/>
+                        {user?.email}
+                    </p>
+                    <Space direction="vertical">
+                        <Text type="secondary" strong>
+                            Registered At:{registeredDate}
+                        </Text>
+                        <Button onClick={onSetIsEditing}>Edit Profile</Button>
+                    </Space>
+                </div>
+            </Col>
+            <Col span={8}>
+                <div className="profile-details">
+                    <Text type="secondary" strong>
+                        Gender
+                    </Text>
+                    <p>{user?.gender}</p>
+                    <Text type="secondary" strong>
+                        Date of Birth
+                    </Text>
+                    <p>{dobDate}</p>
+                    <Text type="secondary" strong>
+                        Blood
+                    </Text>
+                    <p>{user?.bloodType}</p>
+                    <Text type="secondary" strong>
+                        Allergies
+                    </Text>
+                    <List
+                        size="small"
+                        itemLayout={"horizontal"}
+                        dataSource={user?.allergies}
+                        renderItem={(item: string) => <List.Item>{item}</List.Item>}
+                    />
+                    <Text type="secondary" strong>
+                        Medical History
+                    </Text>
+                    <List
+                        size="small"
+                        dataSource={user?.medicalHistory}
+                        renderItem={(item: string) => <List.Item>{item}</List.Item>}
+                    />
+                </div>
+            </Col>
+            <Col span={8}>
+                <div className="profile-history">
+                    {user?.currentMedications && (
+                        <>
+                            <Text type="secondary" strong>
+                                Current Medications
+                            </Text>
+                            <Descriptions
+                                bordered
+                                column={1}
+                                style={{ marginTop: '1rem' }}
+                                items={user?.currentMedications.map((item, index) => ({
+                                    key: index.toString(),
+                                    label: item.medication,
+                                    span: 3,
+                                    children: (
+                                        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                            <Button
+                                                type="default"
+                                                onClick={() => handleSeeDescription(item.medication, item.description)}
+                                            >
+                                                See Description
+                                            </Button>
+                                        </div>
+                                    ),
+                                }))}
+                            />
+                        </>)}
+                </div>
+            </Col>
+        </Row>
+    );
 };
 
 export default UserInfo;
